588b54df4bca6b068f5a1975e7b78027232a6597
Maxim Velesyuk <maxim.velesyuk@areo-gaming.com>
2015-08-13 14:02:44 +0300

    Fixed words format output.

--- lbot.lisp
+++ lbot.lisp
@@ -341,7 +341,7 @@
                     ((optima.ppcre:ppcre "(http[s]?://[\\S]+)" url)
                      (let ((reply 
                             (handler-case (let ((data (readability-parse url)))
-                                            (format nil "~a : ~a words"
+                                            (format nil "~a [~a words]"
                                                     (cdr (assoc :title data))
                                                     (cdr (assoc :word--count data))))
                               (error () (get-http-page-title url)))))
